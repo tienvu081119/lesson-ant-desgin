a6bbc93681cce1965509b3389a9fe9452db18c76
tienvu081119 <tienvu081119@gmail.com>
2020-08-10 21:26:49 +0700

    add Redux

--- src/App.js
+++ src/App.js
@@ -10,11 +10,7 @@ function App() {
       <Layout>
         <Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
           <div className="logo" />
-          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
-            <Menu.Item key="1">nav 1</Menu.Item>
-            <Menu.Item key="2">nav 2</Menu.Item>
-            <Menu.Item key="3">nav 3</Menu.Item>
-          </Menu>
+          <Nav />
         </Header>
         <Content className="site-layout" style={{ padding: '0 1px', marginTop: 64 }}>
 
